a1033de5a396b9ff72d749813f408b92e5262be8
coder026jb <138573706+coder026jb@users.noreply.github.com>
2023-07-07 13:46:14 +0200

    Update aimbot.lua

--- aimbot.lua
+++ aimbot.lua
@@ -14,7 +14,7 @@ a:Remove()
 
 FOV = 180
 HIT_PART = "Head"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/scripts/main/silent_aim_no_actor_bypass.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Spoorloos/scripts/main/silent_aim_no_actor_bypass.lua"))() --thx spoorloos for the silent aim
 getgenv().thing = true;
 	
 while getgenv().thing do wait(1)
